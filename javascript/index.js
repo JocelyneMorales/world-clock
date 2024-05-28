e36ee1f53848b50930084f46852a47897afa04d8
JocelyneMorales <moralesjocelyne18@gmail.com>
2024-05-28 12:33:38 +0200

    Added javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#losAngeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  // berlin
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  let berlinTime = moment().tz("Europe/Berlin");
+  berlinDateElement.innerHTML = berlinTime.format("MMM Do YYYY");
+  berlinTimeElement.innerHTML = berlinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
